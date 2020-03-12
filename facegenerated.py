644667a5622fc2847eaf1de7bf0aba6800e44e50
shfaizan <sentry88prime@gmail.com>
2020-03-12 12:27:29 +0530

    Api Key Removed

--- facegenerated.py
+++ facegenerated.py
@@ -1,6 +1,6 @@
 import urllib.request
 # Enter your API key
-wp = urllib.request.urlopen("https://api.generated.photos/api/v1/faces?api_key=W2V1uxWklMkLudblcx1WOA")
+wp = urllib.request.urlopen("https://api.generated.photos/api/v1/faces?api_key=ADD_YOUR_API_KEY")
 pw = wp.read()
 print(pw)
 #Generate your api key from this website: "https://generated.photos/account#"
